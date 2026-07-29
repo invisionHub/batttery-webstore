7e2bee9a22dc55da7a9a8195a6206861820e2595
kay2-dev <odufotekayode4@gmail.com>
2026-07-29 07:47:30 +0100

    lets try this

--- frontend/features/checkout/hook/useCheckOut.ts
+++ frontend/features/checkout/hook/useCheckOut.ts
@@ -1,21 +1,21 @@
-"use client"
+'use client';
 
-import { useCartStore } from "@/store";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import { useCartStore } from '@/store';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export const useCheckOut = () => {
-    const router = useRouter();
-    const { items, calculateTotals, clearCart } = useCartStore();
-    const { subtotal } = calculateTotals();
-    const [ isLoading, setIsLoading ] = useState(false);
+  const router = useRouter();
+  const { items, calculateTotals, clearCart } = useCartStore();
+  const { subtotal } = calculateTotals();
+  const [isLoading, setIsLoading] = useState(false);
 
-    return {
-        items,
-        subtotal,
-        isLoading,
-        setIsLoading,
-        clearCart,
-        router
-    }
-}
+  return {
+    items,
+    subtotal,
+    isLoading,
+    setIsLoading,
+    clearCart,
+    router,
+  };
+};
--- frontend/features/checkout/view/checkout-view.tsx
+++ frontend/features/checkout/view/checkout-view.tsx
@@ -1,7 +1,6 @@
-
 'use client';
 
-import { useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
   checkoutSchema,
@@ -11,19 +10,16 @@ import {
 import { deliveryDetails } from '../bussiness/delivery-details';
 import { useCheckOut } from '../hook/useCheckOut';
 import { deliveryConfig } from '../constants';
-import {colors} from "../../../constants/theme"
-import { EmprtCart } from '../components/checkout/EmprtCart';
 import { StepIndicator } from '../components/checkout/StepIndicator';
 import { FormErrorFormat } from '../components/checkout/FormErrorFormat';
 import { CheckOutForm } from '../components/checkout/CheckOutForm';
 import { CheckOutLayout } from '../components/checkout/CheckOutLayout';
 
-
 //
 
-export default function CheckoutView () {
-   const { isLoading, items, router, subtotal} = useCheckOut()
-  const {steps} = deliveryConfig
+export default function CheckoutView() {
+  const { isLoading, items, router, subtotal } = useCheckOut();
+  const { steps } = deliveryConfig;
 
   const methods = useForm<CheckoutFormData>({
     resolver: zodResolver(checkoutSchema),
@@ -33,31 +29,30 @@ export default function CheckoutView () {
 
   const deliveryMethod = methods.watch('deliveryMethod');
   const paymentMethod = methods.watch('paymentMethod');
-  // BUSSINESS LOGIC
 
   const total = deliveryDetails(deliveryMethod, subtotal);
 
   // VIEW PAGE
   const onSubmit = methods.handleSubmit(() => {
-     console.log("hello")
+    console.log('hello');
   });
 
   // CART EMPTY COMPONENTS
-  if (items.length === 0) {
-    return <EmprtCart />
-  }
+  // if (items.length === 0) {
+  //   return <EmprtCart />
+  // }
 
   return (
     <CheckOutLayout>
-      <StepIndicator steps={steps} /> 
+      <StepIndicator steps={steps} />
       <FormErrorFormat errors={methods.formState.errors} />
       <CheckOutForm
-        deliveryMethod={ deliveryMethod }
-        paymentMethod={ paymentMethod }
-        isLoading={ isLoading }
-        methods={ methods }
-        onSubmit={ onSubmit }
-        total={ total }
+        deliveryMethod={deliveryMethod}
+        paymentMethod={paymentMethod}
+        isLoading={isLoading}
+        methods={methods}
+        onSubmit={onSubmit}
+        total={total}
       />
     </CheckOutLayout>
   );
